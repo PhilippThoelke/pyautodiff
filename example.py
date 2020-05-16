5fde9635bc13e07061df910e7eab1922d73df200
Philipp Thölke <p.thoelke@gmx.de>
2020-05-16 22:15:23 +0200

    Add an example file

---
+++ example.py
@@ -0,0 +1,18 @@
+from pyautodiff import Variable
+import math
+
+# create variables
+v1 = Variable(3)
+v2 = Variable(5)
+
+# create the computation
+comp1 = 10 * v1
+comp2 = v1 ** 3
+comp3 = v1 * v2
+comp4 = ((v1 ** 8) / (10 * v2 ** 4)) + 3
+
+# get the results and gradients
+print(f'10 * v1 = {float(comp1)} | gradient: [v1: {comp1.gradient(v1)}]')
+print(f'v1 ** 3 = {float(comp2)} | gradient: [v1: {comp2.gradient(v1)}]')
+print(f'v1 * v2 = {float(comp3)} | gradient: [v1: {comp3.gradient(v1)}, v2: {comp3.gradient(v2)}]')
+print(f'((v1 ** 8) / (10 * v2 ** 4)) + 3 = {float(comp4)} | gradient: [v1: {comp4.gradient(v1)}, v2: {comp4.gradient(v2)}]')
