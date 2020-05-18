ee551edfdacc64e21b891add6d94a4bbef327a9a
Philipp Thölke <p.thoelke@gmx.de>
2020-05-18 12:46:08 +0200

    Rename perceptron variable

--- example_perceptron.py
+++ example_perceptron.py
@@ -21,7 +21,8 @@ class Neuron:
 			var.set(float(var) - lr * grad)
 
 if __name__ == '__main__':
-	n = Neuron(2)
+	perceptron = Neuron(2)
+	# example data: logical AND
 	data = [[0, 0], [1, 0], [0, 1], [1, 1]]
 	labels = [0, 0, 0, 1]
 
@@ -30,14 +31,15 @@ if __name__ == '__main__':
 	for epoch in range(n_epochs):
 		epoch_loss = 0
 		for sample, lbl in zip(data, labels):
-			pred = n(sample)
+			pred = perceptron(sample)
 			loss = (pred - lbl) ** 2
 			epoch_loss += float(loss)
-			n.apply_gradient(loss.gradient(n.variables()))
+			perceptron.apply_gradient(loss.gradient(perceptron.variables()))
 
 		if epoch % (n_epochs // 5) == 0:
 			print(f'Loss is {epoch_loss / len(data):.3f} in epoch {epoch + 1}')
 
 	print('\nTesting...')
 	for sample in data:
-		print(f'Prediction for {sample}: {float(n(sample)):.3f} --> {float(n(sample)) > 0.5}')
+		pred = float(perceptron(sample))
+		print(f'Prediction for {sample}: {pred:.3f} --> {pred > 0.5}')
